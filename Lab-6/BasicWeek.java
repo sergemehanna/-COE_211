a6ea67c89f667b4619b240a7b21cf119d37aa62e
sergemehanna <99118595+sergemehanna@users.noreply.github.com>
2022-03-11 17:28:19 +0200

    Add files via upload

---
+++ Lab-6/BasicWeek.java
@@ -0,0 +1,13 @@
+public class BasicWeek {
+	public static void main(String[] args){
+	String day1 = "Monday";
+	String day2 = "Tuesday";
+	string day3 = "Wednesday";
+	String day4 = "Thursday";
+	String day5 = "Friday";
+	String day6 = "Saturday";
+	String day7 = "Sunday";
+
+	System.out.println("The basic days of the week are: " + day1 + " " + day2 + " " + day3 + " " + day4 + " " + day5 + "  " + day6 + " " + day7);
+	}
+}
